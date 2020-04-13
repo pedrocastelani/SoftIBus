0fc2c41a3034d5fb5a6099ca7ff2f64b9ee238c4
pedrocastelani <51168264+pedrocastelani@users.noreply.github.com>
2020-04-13 12:34:27 -0300

    Update SoftIBus10.ino

--- examples/SoftIBus10/SoftIBus10.ino
+++ examples/SoftIBus10/SoftIBus10.ino
@@ -1,17 +1,17 @@
-#include "SoftIBus.h"
-SoftwareSerial IBuspin(2, 3);
+#include "SoftIBus.h"//SoftIBus library
+SoftwareSerial IBuspin(2, 3);//"serial" port on which we will be running the IBus connection. Since to read values we only need the Rx, just connect pin 2 and leave 3 open
 void setup() 
 { 
-  IBuspin.begin(115200);
-  Serial.begin(115200);
-  IBus.begin(IBuspin);
+  IBuspin.begin(115200);//the softserial port has to begin at this baud prior to starting it as the IBus port
+  Serial.begin(115200);//hardserial for debigging
+  IBus.begin(IBuspin);//start the IBus object
 }
 
 void loop() 
 {
   IBus.loop();
-  Serial.print(IBus.readChannel(0), DEC);
-  Serial.print("\t");
+  Serial.print(IBus.readChannel(0), DEC);//get channel 1 (real channel-1= IBus channel) values in DEC format, from 1000 to 2000 if limits set to 100% on transmitter (endpoints are 900 and 2100)
+  Serial.print("\t");//separate the channel values for easy view. Makes the serial plotter (if used) identify each value as another variable
   Serial.print(IBus.readChannel(1), DEC);
   Serial.print("\t");
   Serial.print(IBus.readChannel(2), DEC);
@@ -28,6 +28,6 @@ void loop()
   Serial.print("\t");
   Serial.print(IBus.readChannel(8), DEC);
   Serial.print("\t");
-  Serial.println(IBus.readChannel(9), DEC);
+  Serial.println(IBus.readChannel(9), DEC);//add an "enter" after this value to start a new line next loop
 
 }
