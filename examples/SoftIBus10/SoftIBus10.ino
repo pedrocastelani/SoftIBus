f0ece4cc077c219baaeb9d04ebb882e921058928
pedrocastelani <51168264+pedrocastelani@users.noreply.github.com>
2020-04-12 21:20:32 -0300

    Update SoftIBus10.ino

--- examples/SoftIBus10/SoftIBus10.ino
+++ examples/SoftIBus10/SoftIBus10.ino
@@ -5,30 +5,30 @@ void setup()
 { 
   IBuspin.begin(115200);
   Serial.begin(115200);
-  IBus.begin(IBuspin);
+  SIBus.begin(IBuspin);
 }
 
 void loop() 
 {
   IBus.loop();
-  Serial.print(IBus.readChannel(0), DEC);
+  Serial.print(SIBus.readChannel(0), DEC);
   Serial.print("\t");
-  Serial.print(IBus.readChannel(1), DEC);
+  Serial.print(SIBus.readChannel(1), DEC);
   Serial.print("\t");
-  Serial.print(IBus.readChannel(2), DEC);
+  Serial.print(SIBus.readChannel(2), DEC);
   Serial.print("\t");
-  Serial.print(IBus.readChannel(3), DEC);
+  Serial.print(SIBus.readChannel(3), DEC);
   Serial.print("\t");
-  Serial.print(IBus.readChannel(4), DEC);
+  Serial.print(SIBus.readChannel(4), DEC);
   Serial.print("\t");
-  Serial.print(IBus.readChannel(5), DEC);
+  Serial.print(SIBus.readChannel(5), DEC);
   Serial.print("\t");
-  Serial.print(IBus.readChannel(6), DEC);
+  Serial.print(SIBus.readChannel(6), DEC);
   Serial.print("\t");
-  Serial.print(IBus.readChannel(7), DEC);
+  Serial.print(SIBus.readChannel(7), DEC);
   Serial.print("\t");
-  Serial.print(IBus.readChannel(8), DEC);
+  Serial.print(SIBus.readChannel(8), DEC);
   Serial.print("\t");
-  Serial.println(IBus.readChannel(9), DEC);
+  Serial.println(SIBus.readChannel(9), DEC);
 
 }
