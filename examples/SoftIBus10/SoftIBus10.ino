fa6af7f2161ac1ad9116b548cc39bfd9250e2424
pedrocastelani <51168264+pedrocastelani@users.noreply.github.com>
2020-05-05 11:12:03 -0300

    Update SoftIBus10.ino

--- examples/SoftIBus10/SoftIBus10.ino
+++ examples/SoftIBus10/SoftIBus10.ino
@@ -10,24 +10,24 @@ void setup()
 void loop() 
 {
   IBus.loop();
-  Serial.print(IBus.readChannel(0), DEC);//get channel 1 (real channel-1= IBus channel) values in DEC format, from 1000 to 2000 if limits set to 100% on transmitter (endpoints are 900 and 2100)
+  Serial.print(IBus.readChannel(0));//get channel 1 (real channel-1= IBus channel) values in DEC format, from 1000 to 2000 if limits set to 100% on transmitter (endpoints are 900 and 2100)
   Serial.print("\t");//separate the channel values for easy view. Makes the serial plotter (if used) identify each value as another variable
-  Serial.print(IBus.readChannel(1), DEC);
+  Serial.print(IBus.readChannel(1));
   Serial.print("\t");
-  Serial.print(IBus.readChannel(2), DEC);
+  Serial.print(IBus.readChannel(2));
   Serial.print("\t");
-  Serial.print(IBus.readChannel(3), DEC);
+  Serial.print(IBus.readChannel(3));
   Serial.print("\t");
-  Serial.print(IBus.readChannel(4), DEC);
+  Serial.print(IBus.readChannel(4));
   Serial.print("\t");
-  Serial.print(IBus.readChannel(5), DEC);
+  Serial.print(IBus.readChannel(5));
   Serial.print("\t");
-  Serial.print(IBus.readChannel(6), DEC);
+  Serial.print(IBus.readChannel(6));
   Serial.print("\t");
-  Serial.print(IBus.readChannel(7), DEC);
+  Serial.print(IBus.readChannel(7));
   Serial.print("\t");
-  Serial.print(IBus.readChannel(8), DEC);
+  Serial.print(IBus.readChannel(8));
   Serial.print("\t");
-  Serial.println(IBus.readChannel(9), DEC);//add an "enter" after this value to start a new line next loop
+  Serial.println(IBus.readChannel(9));//add an "enter" after this value to start a new line next loop
 
 }
