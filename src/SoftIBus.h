9585ac0d30ff34fc3e6314cbcc84d59a76617c37
AlexBeem <71223577+AlexBeem@users.noreply.github.com>
2021-11-19 20:11:30 +0300

    Update SoftIBus.h

--- src/SoftIBus.h
+++ src/SoftIBus.h
@@ -24,7 +24,7 @@ private:
   static const uint8_t PROTOCOL_LENGTH = 0x20;
   static const uint8_t PROTOCOL_OVERHEAD = 3; // <len><cmd><data....><chkl><chkh>
   static const uint8_t PROTOCOL_TIMEGAP = 3; // Packets are received very ~7ms so use ~half that for the gap
-  static const uint8_t PROTOCOL_CHANNELS = 10;
+  static const uint8_t PROTOCOL_CHANNELS = 14;
   static const uint8_t PROTOCOL_COMMAND40 = 0x40; // Command is always 0x40
 
   uint8_t state;
